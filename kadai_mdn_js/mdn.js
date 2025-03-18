788f99b89e7bdeaa936fc84f1aae89b1439a518d
ToMaaaN-git <t_at.c_hn.d_te@icloud.com>
2025-03-19 03:20:25 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const getTodayDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0'); 
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}年${month}月${day}日`;
+}
+
+console.log(getTodayDate());
